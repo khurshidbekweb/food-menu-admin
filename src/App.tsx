17df2845b1cfd1f824da081902b2cc731871b6f9
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-11-22 22:38:25 +0500

    add library and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Auth from "./page/auth";
+import Root from "./layouts/Root";
+import Home from "./page/home";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root/>,
+    children: [
+      {
+        index: true,
+        element: <Auth/>
+      },
+      {
+        path: 'home',
+        element: <Home/>
+      }
+    ]
+  }
+])
+
 
 const App = () => {
   return (
     <div>
-      <Auth/>
+      <RouterProvider router={router}/>
     </div>
   );
 };
