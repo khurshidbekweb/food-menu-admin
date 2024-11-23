bf8cd1248332761d3b1ed139a04f3dacdb2a0a58
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-11-23 10:36:26 +0500

    add dialog and scroll

--- src/page/category.tsx
+++ src/page/category.tsx
@@ -1,11 +1,15 @@
 import Navbar from "@/components/navbar/navbar";
+import AddCategory from "@/modal/add-category";
 
 const Category = () => {
     return (
-        <div>
+        <>
             <Navbar/>
-            Category
-        </div>
+            <div className="flex justify-between items-center p-2 md:px-5">
+                <h2 className="text-2xl font-semibold">Category</h2>
+                <AddCategory/>
+            </div>
+        </>
     );
 };
 
