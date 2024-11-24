ad7bcde78ba459aebbcbc95253ac1c2835c65eef
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-11-25 01:11:28 +0500

    delete category

--- src/page/category.tsx
+++ src/page/category.tsx
@@ -8,16 +8,31 @@ import { Table,
     TableRow } from "@/components/ui/table";
 import { IMG_BASE_URL } from "@/constants";
 import AddCategory from "@/modal/add-category";
+import DeleteModal from "@/modal/delete-modal";
 import { useCategoryAll } from "@/querys";
+import { QUERY_KEYS } from "@/querys/query-key";
 import { useStore } from "@/store";
 import { category, Restaurant } from "@/types";
+import { categoryUtils } from "@/utils/category";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const Category = () => {
     const restaurant:Restaurant = JSON.parse(localStorage.getItem('restaurentId'))
-    const categoryes = useCategoryAll(restaurant?._id)?.data
-    console.log(categoryes);
-    
+    const categoryes = useCategoryAll(restaurant?._id)?.data    
     const  {language} = useStore()
+    const queryClient = useQueryClient()
+    const deleteCategory = useMutation({
+    mutationFn: categoryUtils.deleteCategory,
+    onSuccess: () => {
+        toast.success('Muvaffaqiyatli o`chirildi')
+        queryClient.invalidateQueries({queryKey: [QUERY_KEYS.category]})
+     },
+     onError: (err) => {
+        console.log(err);
+        toast.error('Xatolik mavjud')
+     }
+    })
     return (
         <>
             <Navbar />
@@ -30,19 +45,19 @@ const Category = () => {
                     <TableCaption>All language table</TableCaption>
                     <TableHeader>
                         <TableRow>
-                            <TableHead className="w-[100px]">Name</TableHead>
-                            <TableHead>Code</TableHead>
                             <TableHead>Image</TableHead>
+                            <TableHead className="w-[100px]">Name</TableHead>
+                            <TableHead></TableHead>
                             <TableHead className="text-right"></TableHead>
                         </TableRow>
                     </TableHeader>
                     <TableBody>
                         {categoryes?.length && categoryes?.map((res: category) => (
                             <TableRow key={res._id}>
+                                <TableCell><img className="w-[40px] rounded-full" src={`${IMG_BASE_URL}${res.image.image}`} alt="" /></TableCell>
                                 <TableCell className="font-medium">{JSON.parse(res.name)[language.code]}</TableCell>
                                 {/* <TableCell><img src={`${IMG_BASE_URL}${res.image}`} alt="category img" /></TableCell> */}
-                                <TableCell><img className="w-[40px] rounded-full" src={`${IMG_BASE_URL}${res.image.image}`} alt="" /></TableCell>
-                                {/* <TableCell className=""><DeleteModal style="" fn={deleteLanguage.mutate} id={res._id}/></TableCell> */}
+                                <TableCell className=""><DeleteModal style="" fn={deleteCategory.mutate} id={res._id}/></TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
