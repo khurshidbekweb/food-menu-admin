908b12c89cc285d8a75feafbb12d4625fa2fe6cc
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-11-28 17:10:13 +0500

    add all language

--- src/components/navbar/user-settings.tsx
+++ src/components/navbar/user-settings.tsx
@@ -6,19 +6,21 @@ import { DropdownMenu,
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger, } from '../ui/dropdown-menu';
+import { useTranslation } from 'react-i18next';
 
 const UserSettings = () => {
   const logOut = () => {
     localStorage.clear()
     window.location.reload()
   }
+  const {t} = useTranslation()
     return (
         <DropdownMenu>
         <DropdownMenuTrigger><User/></DropdownMenuTrigger>
         <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuLabel>{t("user_profile")}</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={logOut} className='bg-red-400 text-white cursor-pointer hover:bg-red-400'>Log out</DropdownMenuItem>
+          <DropdownMenuItem onClick={logOut} className='bg-red-400 text-white cursor-pointer hover:bg-red-400'>{t("user_log_out")}</DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
       
